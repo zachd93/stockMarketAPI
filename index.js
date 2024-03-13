da35977917abfad33490ac1283f30d856d8d2976
zachd93 <zachdebolt1993@gmail.com>
2024-03-13 12:25:08 -0700

    Almost finished

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const API_URL = "https://financialmodelingprep.com/api/v3/profile/";
 const envVar = dotenv.config();
 const API_KEY = process.env.API_KEY; // Replace with your own personal API Key or the app won't work.
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
@@ -24,8 +24,7 @@ app.get("/", async (req, res) => {
 app.post("/search", async (req, res) => {
     try {
         const data = await axios.get(API_URL + req.body.name + "?" + API_KEY);
-        console.log(data)
-        res.render("index.ejs", { data: JSON.stringify(data.data[0].price) });
+        res.render("index.ejs", { data: JSON.stringify(data.data[0].price), name: req.body.name });
     } catch (error) {
         //
     }
